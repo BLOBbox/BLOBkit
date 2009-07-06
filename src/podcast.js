83a24ddab404fe37186054d3888a0de758f1ed62
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2009-07-06 14:27:45 +0000

    Added the Content part of the API
    git-svn-id: http://blobkit.googlecode.com/svn/branches/BLOBkit_1.2.0@35 fcc7d3b4-6095-11de-a55d-e76e375ae706

--- src/podcast.js
+++ src/podcast.js
@@ -492,69 +492,24 @@ TVB.podcast.getFeedContentByID = function(feedID) {
 	}
 }
 
-
-
-
-// STILL TO BE REFACTORED
-
-/*
- * Refreshes the content of the feeds in memory
- * @method refresh
- * @private
- * @return {Boolean}
- */
-/*
-TVB.podcast.refresh = function() {
-	try {
-		TVB.log("Podcast: refresh");
-		if (TVB.podcast.mgr == null) {
-			throw {message: "Not inited"};
-		}
-		TVB.podcast.fh = TVB.podcast.mgr.getAllFeeds();
-		for (var i in TVB.podcast.fh) {
-			var fho = TVB.podcast.fh[i];
-			var fhd = {
-				ID: fho.getID(),
-				title: fho.getTitle(),
-				isHidden: fho.isHidden(),
-				hasContents: fho.hasContents(),
-				lastBuildDate: fho.getLastBuildDate(),
-				pubDate: fho.getPubDate(),
-				timeToLive: fho.getTimeToLive(),
-				isTimeToLiveInfinite: fho.isTimeToLiveInfinite()
-			}
-			TVB.podcast.feeds[fho.getID()] = fhd;
-		}
-		return true;
-	} catch (e) {
-		TVB.error("podcast.refresh: " + e.message);
-		throw e;
-	}
-}
-*/
-
 /**
  * Returns an URI from an ID
  * @method getUriByID
- * @param {String} feed_id The ID of a feed
- * @param {String} id The ID of a content
+ * @param {String} feedID The ID of a feed
+ * @param {String} contentID The ID of a content
+ * @return {String} an URI, null if not found
  */
-TVB.podcast.getUriByID = function(feed_id, id) {
+TVB.podcast.getUriByID = function(feedID, contentID) {
 	try {
-		TVB.log("Podcast: get uri by id " + id);
+		TVB.log("Podcast: getUriByID(" + feedID + ", " + contentID + ")");
 		if (TVB.podcast.mgr == null) {
-			throw {message: "Not inited"};
+			TVB.podcast.init();
 		}
-		var data = TVB.podcast.getFeedContentByID(feed_id);
-		for (var i in data) {
-			//TVB.log("Podcast: examinating " + data[i].ID + " = " + id + "...");
-			if (data[i].ID == id) {
-				return data[i].uri;
-			}
-		}
-		return false;
+		var fho = TVB.podcast.mgr.getFeedByID(feedID);
+		var cho = fho.getContentByID(contentID); 
+		return cho.getURI();
 	} catch (e) {
-		TVB.error("podcast.getUriByID: " + e.message);
+		TVB.error("Podcast: getUriByID: " + e.message);
 		throw e;
 	}
 }
