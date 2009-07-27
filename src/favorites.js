4d0778b6c45f0e5525dacc6d3caf1dd4b8ccd01d
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2009-07-27 15:48:26 +0000

    New module: favorites
    git-svn-id: http://blobkit.googlecode.com/svn/trunk@75 fcc7d3b4-6095-11de-a55d-e76e375ae706

---
+++ src/favorites.js
@@ -0,0 +1,69 @@
+/**
+ * <h1>Favorites Manager for BLOBbox</h1>
+ * 
+ * @module favorites
+ * @namespace TVB
+ * @title Favorites Manager
+ * @requires tvblob
+ * @author Francesco Facconi francesco.facconi@tvblob.com
+ */
+
+/**
+ * Object for handling the Favorites Manager
+ * @class favorites
+ * @static
+ * @namespace TVB
+ */
+TVB.favorites = {}
+
+/**
+ * Set the URL for favorite callback
+ * @method set
+ * @param {String} uri A complete URL that will be saved in the bookmark when the favorites button is pushed
+ */
+TVB.favorites.set = function(uri) {
+	try {
+		TVB.log("Favorites: set('" + uri + "')");
+		if (typeof uri != 'string') {
+			throw TypeError;
+		}
+		TVB.favorites.handler.setURI(encodeURI(uri));
+	} catch (e) {
+		TVB.error("Favorites: set: " + e.message);
+		throw e;
+	}
+}
+
+/**
+ * Configure the callback the produce the favorite url<br />
+ * This is an alternative way to use this API
+ * @method setProducer
+ * @parameter {String} functionName the name of a function (as string!) to be called when favorite button is pushed; the function should return a string (the url to be saved with the bookmark). Only valid urls will be accepted.
+ */
+TVB.favorites.setProducer = function(functionName) {
+	try {
+		TVB.log("Favorites: setProducer('" + functionName + "')");
+		if (typeof functionName != 'string') {
+			throw TypeError;
+		}
+		TVB.favorites.handler.setProducer(functionName);
+	} catch (e) {
+		TVB.error("Favorites: setProducer: " + e.message);
+		throw e;
+	}
+}
+
+/*
+ * Initialization of the favorites module
+ */
+try {
+	/**
+	 * Hander for favorites events
+	 * @private
+	 */
+	TVB.favorites.handler = new BlobFavoritesHandler();
+	TVB.favorites.set(location.href);
+} catch (e) {
+	TVB.error("Favorites: " + e.message);
+}
+
