a0c3fc8b4fb9e44b74998ca0354cecbc92596f82
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2009-08-24 09:01:45 +0000

    New Exceptions file, it will contain all exceptions used in the library
    git-svn-id: http://blobkit.googlecode.com/svn/trunk@87 fcc7d3b4-6095-11de-a55d-e76e375ae706

---
+++ src/exceptions.js
@@ -0,0 +1,15 @@
+/*
+ * Default Exceptions used:
+ * - EvalError
+ * - RangeError
+ * - ReferenceError
+ * - SyntaxError
+ * - TypeError
+ * - URIError
+ * 
+ * Use try {} catch (e) {};
+ * or try {} catch (e instanceOf errorType) {} catch (e) {};
+ * to evaluate functions
+ */
+
+var InitError = new Error ("Current module has not been inited");
