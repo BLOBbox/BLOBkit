ec32c01487bc52e43fd2182f2e6af5b77cc0759f
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2009-10-05 16:28:50 +0000

    The library supports php 5.0.0 with sqlite2 and php 5.3.0 with sqlite3
    git-svn-id: http://blobkit.googlecode.com/svn/trunk@126 fcc7d3b4-6095-11de-a55d-e76e375ae706

--- contrib/blobtv/admin/common.php
+++ contrib/blobtv/admin/common.php
@@ -7,6 +7,16 @@
 
 $db_conn = null;
 
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+	if (version_compare(PHP_VERSION, '5.0.0', '<')) {
+		die ("ERROR: PHP 5.0.0 or greater is required");
+	} else {
+		$sqlite3 = false;
+	}
+} else {
+	$sqlite3 = true;
+}
+
 $available_languages = Array('it', 'en');
 
 if ($force_language) {
@@ -22,54 +32,88 @@ if ($force_language) {
 
 function open_db_connection() {
 	global $db_conn, $data_file;
-	if ($db_conn = new SQLite3($data_file)) {
-		if ($res = @$db_conn->query("SELECT id FROM items LIMIT 1")) {
-			// db already exists
-			// update db data
-		} else {
-			// initialize new db
-			$sql = "CREATE TABLE items (
-				id INTEGER PRIMARY KEY ASC,
-				title TEXT,
-				subtitle TEXT,
-				description TEXT,
-				file_name TEXT,
-				thumbnail TEXT,
-				is_published BOOLEAN DEFAULT 0,
-				date_published DATETIME,
-				last_modified DATETIME,
-				hit INT DEFAULT 0
-			)";
-			if (!@$db_conn->exec($sql)) {
-				show_error(ERR_DB_CREATION);
+	$sql_create = "CREATE TABLE items (
+		id INTEGER PRIMARY KEY ASC,
+		title TEXT,
+		subtitle TEXT,
+		description TEXT,
+		file_name TEXT,
+		thumbnail TEXT,
+		is_published BOOLEAN DEFAULT 0,
+		date_published DATETIME,
+		last_modified DATETIME,
+		hit INT DEFAULT 0
+	)";
+	$sql_fetch = "SELECT id FROM items LIMIT 1";
+				
+	if ($sqlite3) {
+		// SQlite 3 edition
+		if ($db_conn = new SQLite3($data_file)) {
+			if ($res = @$db_conn->query($sql_fetch)) {
+				// db already exists
+				// update db data
+			} else {
+				// initialize new db
+				if (!@$db_conn->exec($sql_create)) {
+					show_error(ERR_DB_CREATION);
+				}
 			}
-			
+			return true;
+		} else {
+			show_error(ERR_DB_OPEN);
+			return false;
 		}
-		return true;
 	} else {
-		show_error(ERR_DB_OPEN);
-		return false;
+		// SQlite 2 edition
+		if ($db_conn = new SQLiteDatabase($data_file)) {
+			if ($res = @$db_conn->query($sql_fetch)) {
+				// db already exists
+				// update db data
+			} else {
+				// initialize new db
+				if (!@$db_conn->queryExec($sql_create)) {
+					show_error(ERR_DB_CREATION);
+				}
+			}
+			return true;
+		} else {
+			show_error(ERR_DB_OPEN);
+			return false;
+		}		
 	}
 }
 
 function close_db_connection() {
 	global $db_conn;
-	$id = @$db_conn->close();
+	if ($sqlite3) {
+		$id = $db_conn->close();
+	}
 	return true;
 }
 
 function add_new_video($title, $subtitle, $description, $data) {
 	global $db_conn;
 	
-	$title = $db_conn->escapeString($title);
-	$subtitle = $db_conn->escapeString($subtitle);
-	$description = $db_conn->escapeString($description);
-	$data = $db_conn->escapeString($data);
+	if ($sqlite3) {
+		$title = $db_conn->escapeString($title);
+		$subtitle = $db_conn->escapeString($subtitle);
+		$description = $db_conn->escapeString($description);
+		$data = $db_conn->escapeString($data);
+	} else {
+		$title = sqlite_escape_string($title);
+		$subtitle = sqlite_escape_string($subtitle);
+		$description = sqlite_escape_string($description);
+		$data = sqlite_escape_string($data);
+	}
 	
 	$sql  = "INSERT INTO items (title, subtitle, description, file_name, thumbnail, date_published, last_modified, hit, is_published) ";
 	$sql .=" VALUES('$title', '$subtitle', '$description', NULL, NULL, '$data', 'now', 0, 'false')"; 
 	
-	@$db_conn->exec($sql);
+	if ($sqlite3) {
+		@$db_conn->exec($sql);
+	} else {
+		@$db_conn->queryExec($sql);
+	}
 	$id = $db_conn->lastInsertRowID();
 	return $id;
 }
@@ -83,69 +127,130 @@ function load_video_list($all = false) {
 	}
 	$results = @$db_conn->query($sql);
 	$dat = Array();
-	while ($row = $results->fetchArray()) {
-		$newdat = Array();
-		$newdat['id'] = $row['id'];
-		$newdat['title'] = $row['title'];
-		$newdat['subtitle'] = $row['subtitle'];
-		$newdat['description'] = $row['description'];
-		$newdat['file_name'] = $base_video_url . "/" . $row['file_name'];
-		$newdat['thumbnail'] = $base_thumb_url . "/" . $row['thumbnail'];
-		$newdat['is_published'] = $row['is_published'];
-		$newdat['date_published'] = $row['date_published'];
-		$newdat['last_modified'] = $row['last_modified'];
-		$newdat['hit'] = $row['hit'];
-		array_push($dat, $newdat);
+	if ($sqlite3) {
+		while ($row = $results->fetchArray()) {
+			$newdat = Array();
+			$newdat['id'] = $row['id'];
+			$newdat['title'] = $row['title'];
+			$newdat['subtitle'] = $row['subtitle'];
+			$newdat['description'] = $row['description'];
+			$newdat['file_name'] = $base_video_url . "/" . $row['file_name'];
+			$newdat['thumbnail'] = $base_thumb_url . "/" . $row['thumbnail'];
+			$newdat['is_published'] = $row['is_published'];
+			$newdat['date_published'] = $row['date_published'];
+			$newdat['last_modified'] = $row['last_modified'];
+			$newdat['hit'] = $row['hit'];
+			array_push($dat, $newdat);
+		}
+		$results->finalize();
+	} else {
+		while ($row = $results->fetch(SQLITE_ASSOC)) {
+			$newdat = Array();
+			$newdat['id'] = $row['id'];
+			$newdat['title'] = $row['title'];
+			$newdat['subtitle'] = $row['subtitle'];
+			$newdat['description'] = $row['description'];
+			$newdat['file_name'] = $base_video_url . "/" . $row['file_name'];
+			$newdat['thumbnail'] = $base_thumb_url . "/" . $row['thumbnail'];
+			$newdat['is_published'] = $row['is_published'];
+			$newdat['date_published'] = $row['date_published'];
+			$newdat['last_modified'] = $row['last_modified'];
+			$newdat['hit'] = $row['hit'];
+			array_push($dat, $newdat);
+		}
 	}
-	$results->finalize();
 	return $dat;
 }
 
 function insert_thumb_url($id, $url) {
 	global $db_conn;
-	$url = $db_conn->escapeString($url);
-	return @$db_conn->exec("UPDATE items SET thumbnail = '$url' WHERE id = $id");
+	if ($sqlite3) {
+		$url = $db_conn->escapeString($url);
+		$res = @$db_conn->exec("UPDATE items SET thumbnail = '$url' WHERE id = $id");
+	} else {
+		$url = sqlite_escape_string($url);
+		$res = @$db_conn->queryExec("UPDATE items SET thumbnail = '$url' WHERE id = $id");
+	}
+	return $res;
 }
 
 function insert_video_url($id, $url) {
 	global $db_conn;
-	$url = $db_conn->escapeString($url);
-	return @$db_conn->exec("UPDATE items SET file_name = '$url' WHERE id = $id");
+	if ($sqlite3) {
+		$url = $db_conn->escapeString($url);
+		$res = @$db_conn->exec("UPDATE items SET file_name = '$url' WHERE id = $id");
+	} else {
+		$url = sqlite_escape_string($url);
+		$res = @$db_conn->queryExec("UPDATE items SET file_name = '$url' WHERE id = $id");
+	}
+	return $res;
 }
 
 function aggiorna_video($id, $title, $subtitle, $date, $description) {
 	global $db_conn;
-	$title = $db_conn->escapeString($title);
-	$subtitle = $db_conn->escapeString($subtitle);
-	$description = $db_conn->escapeString($description);
-	return @$db_conn->exec("UPDATE items SET title = '$title', subtitle = '$subtitle', date_published = '$date', description = '$description' WHERE id = $id");
+	if ($sqlite3) {
+		$title = $db_conn->escapeString($title);
+		$subtitle = $db_conn->escapeString($subtitle);
+		$description = $db_conn->escapeString($description);
+		$res = @$db_conn->exec("UPDATE items SET title = '$title', subtitle = '$subtitle', date_published = '$date', description = '$description' WHERE id = $id");
+	} else {
+		$title = sqlite_escape_string($title);
+		$subtitle = sqlite_escape_string($subtitle);
+		$description = sqlite_escape_string($description);
+		$res = @$db_conn->queryExec("UPDATE items SET title = '$title', subtitle = '$subtitle', date_published = '$date', description = '$description' WHERE id = $id");
+	}
+	return $res;
 }
 
 function get_video_details($id) {
 	global $db_conn;
-	return @$db_conn->querySingle("SELECT * FROM items WHERE id = $id", true);
+	if ($sqlite3) {
+		$res = @$db_conn->querySingle("SELECT * FROM items WHERE id = $id", true); 
+	} else {
+		$res = @$db_conn->singleQuery("SELECT * FROM items WHERE id = $id", true);
+	}
+	return $res;
 }
 
 function delete_video($id) {
 	global $db_conn, $base_video_folder, $base_thumb_folder;
-	$results = @$db_conn->querySingle("SELECT file_name, thumbnail FROM items WHERE id = $id", true);
+	if ($sqlite3) {
+		$results = @$db_conn->querySingle("SELECT file_name, thumbnail FROM items WHERE id = $id", true);
+	} else {
+		$results = @$db_conn->singleQuery("SELECT file_name, thumbnail FROM items WHERE id = $id", true);
+	}
 	if (!@unlink($base_video_folder . "/" . $results['file_name'])) {
 		print "<p>" . str_replace("%%FILE%%", $results['file_name'], ERR_UNABLE_TO_DEL) . "</p>";
 	}
 	if (!@unlink($base_thumb_folder . "/" . $results['thumbnail'])) {
 		print "<p>" . str_replace("%%FILE%%", $results['thumbnail'], ERR_UNABLE_TO_DEL) . "</p>";
 	}
-	return @$db_conn->exec("DELETE FROM items WHERE id = $id");
+	if ($sqlite3) {
+		$res = @$db_conn->exec("DELETE FROM items WHERE id = $id");
+	} else {
+		$res = @$db_conn->queryExec("DELETE FROM items WHERE id = $id");
+	}
+	return $res;
 }
 
 function activate_video($id) {
 	global $db_conn;
-	return $db_conn->exec("UPDATE items SET is_published = 1 WHERE id = $id");
+	if ($sqlite3) {
+		$res = @$db_conn->exec("UPDATE items SET is_published = 1 WHERE id = $id");
+	} else {
+		$res = @$db_conn->queryExec("UPDATE items SET is_published = 1 WHERE id = $id");
+	}
+	return $res;
 }
 
 function deactivate_video($id) {
 	global $db_conn;
-	return $db_conn->exec("UPDATE items SET is_published = 0 WHERE id = $id");
+	if ($sqlite3) {
+		$res = @$db_conn->exec("UPDATE items SET is_published = 0 WHERE id = $id");
+	} else {
+		$res = @$db_conn->queryExec("UPDATE items SET is_published = 0 WHERE id = $id");
+	}
+	return $res;
 }
 
 function show_error($message) {
