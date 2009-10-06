11fa05e1a68b84a6b8d87d0f9431ebf0408dccb8
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2009-10-06 15:37:13 +0000

    Another update to zIndexes to fix bu 2944
    git-svn-id: http://blobkit.googlecode.com/svn/trunk@128 fcc7d3b4-6095-11de-a55d-e76e375ae706

--- src/player.js
+++ src/player.js
@@ -1030,7 +1030,7 @@ TVB.player.showBufferingMessage = function() {
 			div.style.left = '30px';
 			div.style.padding = 0;
 			div.style.margin = 0;
-			div.style.zIndex = '8000';			
+			div.style.zIndex = '10001';			
 			document.body.appendChild(div);
 		}
 		var ico = null;
@@ -1065,7 +1065,7 @@ TVB.player.showStartingPlaybackMessage = function() {
 			div.style.left = '30px';
 			div.style.padding = 0;
 			div.style.margin = 0;
-			div.style.zIndex = '8001';			
+			div.style.zIndex = '10002';			
 			document.body.appendChild(div);
 		}
 		var ico = null;
@@ -1104,7 +1104,7 @@ TVB.player.showUnableToPlayMessage = function() {
 			div.style.padding = '0';
 			div.style.paddingLeft = '10px';
 			div.style.paddingRight = '10px';
-			div.style.zIndex = '8002';
+			div.style.zIndex = '10003';
 
 			div.innerHTML = '';
 	
