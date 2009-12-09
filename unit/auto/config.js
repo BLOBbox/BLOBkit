281d87d6ecee7c978388887fed764fb6ca8d5d67
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2009-12-09 16:58:28 +0000

    Splitted the config.js in config.js (dynamic) and config.static.js (static properties)
    git-svn-id: http://blobkit.googlecode.com/svn/trunk@150 fcc7d3b4-6095-11de-a55d-e76e375ae706

--- unit/auto/config.js
+++ unit/auto/config.js
@@ -2,26 +2,12 @@
  * Config this values for every test-run
  */
 
-//showDebugMessages = false;
-showDebugMessages = true;
-
-//isUntrustedPage = false;
-isUntrustedPage = true;
-
-featureVersionBlobPlayer = "1.1";
-featureVersionBlobRemoteControl = "1.0";
-featureVersionHTTPRelay = "1.0";
-featureVersionPushVodFeedsManager = "1.1";
-featureVersionTvblob = "1.7";
+// DYNAMIC REQUIREMENTS
 firmwareVersion = "BLOBBOX/std 0.7.43 #641";
 languageCode = "en";
 productName = "BLOBBOX_NOENC";
 productVersion = "1.51.0.7BF.1";
 SMOJVersion = "1.51.0.7BF.1";
 SMOSVersion = "2.14.2-wifi-otus [2382/wifi-otus]";
-tvblobNumber = "02226719628";
 videoSystem = "PAL";
-
-
-
-// inserisco START e DONE nei log del test
+tvblobNumber = "02226719628";
---
+++ unit/auto/config.static.js
@@ -0,0 +1,10 @@
+// STATIC REQUIREMENTS
+showDebugMessages = true;
+isUntrustedPage = true;
+
+//http://bigone/wiki/doku.php?id=feature
+featureVersionBlobPlayer = "1.1";
+featureVersionBlobRemoteControl = "1.0";
+featureVersionHTTPRelay = "1.0";
+featureVersionPushVodFeedsManager = "1.1";
+featureVersionTvblob = "1.7";
