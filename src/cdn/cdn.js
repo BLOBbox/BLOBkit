4c752bac44dbb7cf63a4ca5042a0e020fd25ee62
facconi <facconi@fcc7d3b4-6095-11de-a55d-e76e375ae706>
2010-05-10 15:06:19 +0000

    Fixed text code
    git-svn-id: http://blobkit.googlecode.com/svn/trunk@196 fcc7d3b4-6095-11de-a55d-e76e375ae706

--- src/cdn/cdn.js
+++ src/cdn/cdn.js
@@ -1,6 +1,6 @@
 /**
  * <h1>CDN Manager for BLOBbox</h1>
- * 
+ *
  * @module cdn
  * @namespace TVB
  * @title CDN Manager
@@ -16,18 +16,18 @@
  * @namespace TVB
  */
 TVB.cdn = {
-		provider: null,
-		providerConnector:null
+    provider: null,
+    providerConnector: null
 };
 
 /**
  * Returns the rtmp url of a given stream for a given cdnType. If the cdn is not specified the algorithm tries
- * to guess it decoding the url 
+ * to guess it decoding the url
  * @param {String} fileUrl The url of the stream
  * @param {String} cdnType Optional parameter; is the cdn type name
- * @return {String}ÊThe real playback url, null if not available
- **/
-TVB.cdn.getRtmpUrl = function(fileUrl, cdnType){
+ * @return {String} The real playback url, null if not available
+ */
+TVB.cdn.getRtmpUrl = function(fileUrl, cdnType) {
     try {
         if (cdnType === null) {
             TVB.cdn.detectProvider(fileUrl);
@@ -75,7 +75,7 @@ TVB.cdn.detectProvider = function(url){
 
 /**
  * Selects the connector to use by the given cdn type
- * @param {String}ÊcdnType 
+ * @param {String} cdnType
  */
 TVB.cdn.selectConnector = function(cdnType){
     try {
@@ -97,7 +97,7 @@ TVB.cdn.selectConnector = function(cdnType){
             case "lv3:":
                 TVB.cdn.provider = "Level3";
                 TVB.cdn.providerConnector = TVB.cdn.level3Connector;
-				break;
+                break;
             case "fms:":
                 break;
             default:
@@ -122,15 +122,15 @@ TVB.cdn.velocixConnector = {
             var nodeList = xml.getAllChildNodes();
             for (var el in nodeList) {
                 if (nodeList[el].getName() == "streamname") {
-					file = nodeList[el].getValue();
-				}
+                    file = nodeList[el].getValue();
+                }
                 if (nodeList[el].getName() == "rtmpurl") {
-					server = nodeList[el].getValue();
-				}
+                    server = nodeList[el].getValue();
+                }
             }
             if (server === "" || file === "") {
-				return;
-			}
+                return;
+            }
             return server + "/" + file;
         } catch (e) {
             TVB.log(e);
@@ -140,48 +140,49 @@ TVB.cdn.velocixConnector = {
 };
 
 TVB.cdn.level3Connector = {
-	getUrl:function(url){		
-		try{
-			
-			var start = url.indexOf("/") + 1;
-			var med = url.indexOf("/", start) + 1;
-			var end = url.indexOf("/", med);
-			
-			var serverUrl = "";
-			if (end != -1) {
-				serverUrl = "rtmp://" + url.substring(start, end);
-			} else {
-				serverUrl = start === 0 ? "" : "rtmp://" + url.substring(start, med);
-			}
-			start = url.indexOf("/") + 1;
-			med  = url.indexOf("/", start) + 1;
-			end = url.indexOf("/", med);
-			file = start === 0 ? url : url.substring(end + 1, url.length);
-			
-			if (file.indexOf("mp4:") == -1 &&  (file.indexOf(".mp4") != -1 || 
-					file.indexOf(".mov") != -1 ||
-					file.indexOf(".mpg") != -1 || 
-					file.indexOf(".f4v") != -1 || 
-					file.indexOf(".m4v") != -1 )) {
-					file = "mp4:" + file;
-			}
-			return serverUrl + "/" + file;
-		}catch(e){
-			TVB.error(e);
-			return null;
-		}
-	}	
+    getUrl: function(url){
+        try {
+        
+            var start = url.indexOf("/") + 1;
+            var med = url.indexOf("/", start) + 1;
+            var end = url.indexOf("/", med);
+            
+            var serverUrl = "";
+            if (end != -1) {
+                serverUrl = "rtmp://" + url.substring(start, end);
+            } else {
+                serverUrl = start === 0 ? "" : "rtmp://" + url.substring(start, med);
+            }
+            start = url.indexOf("/") + 1;
+            med = url.indexOf("/", start) + 1;
+            end = url.indexOf("/", med);
+            file = start === 0 ? url : url.substring(end + 1, url.length);
+            
+            if (file.indexOf("mp4:") == -1 &&
+            (file.indexOf(".mp4") != -1 ||
+            file.indexOf(".mov") != -1 ||
+            file.indexOf(".mpg") != -1 ||
+            file.indexOf(".f4v") != -1 ||
+            file.indexOf(".m4v") != -1)) {
+                file = "mp4:" + file;
+            }
+            return serverUrl + "/" + file;
+        } catch (e) {
+            TVB.error(e);
+            return null;
+        }
+    }
 };
 
-/**
- * <smil>
-	<head>
-		<meta base="rtmp://fl9.maelstrom.jet-stream.nl:1935/vod/" />
-	</head>
-	<body>
-		<video src="vdoxadmin/ws-tvblob/ratatouille_480p.mov" />
-	</body>
-</smil>
+/*
+ <smil>
+ <head>
+ <meta base="rtmp://fl9.maelstrom.jet-stream.nl:1935/vod/" />
+ </head>
+ <body>
+ <video src="vdoxadmin/ws-tvblob/ratatouille_480p.mov" />
+ </body>
+ </smil>
  */
 TVB.cdn.smilConnector = {
     getUrl: function(url){
@@ -215,69 +216,64 @@ TVB.cdn.smilConnector = {
                 }
             }
             if (server === "" || file === "") {
-				return;
-			}
+                return;
+            }
             return server + "/" + file;
         } catch (e) {
             TVB.log(e);
             return null;
         }
-    } 
+    }
 };
 
-/**
- * 
- * <playlist version="1">
-
-	<trackList>
-
-		<track>
-			<title/>
-			<creator/>
-			<location>vdoxadmin/ws-tvblob/ratatouille_480p.mov</location>
-			<meta rel="streamer">rtmp://fl9.maelstrom.jet-stream.nl:1935/vod/</meta>
-			<meta rel="type">rtmp</meta>
-		</track>
-	</trackList>
-	</playlist>
+/*
+ <playlist version="1">
+ <trackList>
+ <track>
+ <title/>
+ <creator/>
+ <location>vdoxadmin/ws-tvblob/ratatouille_480p.mov</location>
+ <meta rel="streamer">rtmp://fl9.maelstrom.jet-stream.nl:1935/vod/</meta>
+ <meta rel="type">rtmp</meta>
+ </track>
+ </trackList>
+ </playlist>
  */
 TVB.cdn.xspfConnector = {
-		getUrl: function(url){
-			try{
-				var req = new HTTPRelay();
-				
-				var res = req.get(url);
-				var xml = tvblob.parseXml(res);
-				var server = new Array();
-				var file = new Array();
-				
-				var nodeList = xml.getChildNode("trackList").getAllChildNodes();
-				var cont = 0;
-				for(var el in nodeList){ //el = track
-					if(nodeList[el].getName() == "track"){
-						var sub = nodeList[el].getAllChildNodes();	
-						for(var s in sub){
-							
-							if(sub[s].getName() == "location"){
-								file[cont] = sub[s].getValue();
-							}
-							else if(sub[s].getName() == "meta"){
-								if(sub[s].getAttributeValue("rel") == "streamer"){
-									server[cont] = sub[s].getValue();
-								}
-							}
-						}
-						cont++;
-					}
-				}
-				if (server[0] === "" || file[0] === "") {
-					return;
-				}
-				return server[0] + "/" + file[0];
-			}catch(e){
-				TVB.log(e);
-				return null;
-			}
-		}
+    getUrl: function(url){
+        try {
+            var req = new HTTPRelay();
+            
+            var res = req.get(url);
+            var xml = tvblob.parseXml(res);
+            var server = new Array();
+            var file = new Array();
+            
+            var nodeList = xml.getChildNode("trackList").getAllChildNodes();
+            var cont = 0;
+            for (var el in nodeList) { //el = track
+                if (nodeList[el].getName() == "track") {
+                    var sub = nodeList[el].getAllChildNodes();
+                    for (var s in sub) {
+                    
+                        if (sub[s].getName() == "location") {
+                            file[cont] = sub[s].getValue();
+                        } else if (sub[s].getName() == "meta") {
+                            if (sub[s].getAttributeValue("rel") == "streamer") {
+                                server[cont] = sub[s].getValue();
+                            }
+                        }
+                    }
+                    cont++;
+                }
+            }
+            if (server[0] === "" || file[0] === "") {
+                return;
+            }
+            return server[0] + "/" + file[0];
+        } catch (e) {
+            TVB.log(e);
+            return null;
+        }
+    }
 };
-
